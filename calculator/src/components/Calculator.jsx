aee0e4bcc4e265378fe82f5f90c760980a1c2912
Viviana <40200339@esmad.ipp.pt>
2025-12-17 18:51:44 +0000

    added the logic which stops the user from being able to add numbers and symbols to the equation after already clicking on the "=" button, therefore ending the equation.
    added the functionality to the "AC" button which is to restart everything.

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -6,18 +6,21 @@ import Keys from "./Keys.jsx";
 export default function Calculator() {
     const [equation, setEquation] = useState([]); //stores all the numbers and symbols of the equation
     const [dotLocked, setDotLocked] = useState(false); //turns "true" if the "." button has already been clicked for the number currently being typed
+    const [isEquationOver, setIsEquationOver] = useState(false); //turns "true" if equation is over
 
     /**
      * when a button of a number is clicked, this function adds the number the "equation" array that is both used to, in the future, count the result of the equation, and also to add the numbers and operators to the screen display
      * @param {*} nbr number of the button clicked
      */
     function nbrClicked(nbr) {
-        if (nbr != ".") {
-            setEquation(prevEquation => [...prevEquation, nbr]) // pushes "nbr" to the useState "equation" array
-        } else {
-            if (nbr == "." && !dotLocked) {
-                setDotLocked(true);
+        if (!isEquationOver) {
+            if (nbr != ".") {
                 setEquation(prevEquation => [...prevEquation, nbr]) // pushes "nbr" to the useState "equation" array
+            } else {
+                if (nbr == "." && !dotLocked) {
+                    setDotLocked(true);
+                    setEquation(prevEquation => [...prevEquation, nbr]) // pushes "nbr" to the useState "equation" array
+                }
             }
         }
         
@@ -33,11 +36,13 @@ export default function Calculator() {
     function symbolClicked(symbol) {
         console.log(symbol);
 
-        if (equation.length > 0) { //the first character of the equation CAN'T be a symbol
+        if (equation.length > 0 && !isEquationOver) { //the first character of the equation CAN'T be a symbol AND if the equation isn't over yet
             if (symbol.name == "Equal") { //if the symbol pressed is "="...
                 setEquation(prevEquation =>
                     [...prevEquation, `=${eval(prevEquation.join(''))}`] //adds to the array of the equation "=" and its result
                 )
+
+                setIsEquationOver(true);
             } else {
                 if (equation[equation.length-1] != "*"
                     && equation[equation.length-1] != "/"
@@ -58,9 +63,18 @@ export default function Calculator() {
         }
     }
 
+    /**
+     * restarts the equation when button "AC" is clicked
+     */
+    function acClicked() {
+        setEquation([]);
+        setDotLocked(false);
+        setIsEquationOver(false);
+    }
+
     return (
         <main>
-            <Screen screenDisplay={equation.length == 0 ? "_" : equation.join('')}/>
+            <Screen screenDisplay={equation.length == 0 ? "_" : equation.join('')} acClicked={acClicked}/>
             <Keys nbrClicked={nbrClicked} symbolClicked={symbolClicked}/>
         </main>
     )
--- calculator/src/components/Screen.jsx
+++ calculator/src/components/Screen.jsx
@@ -1,11 +1,11 @@
-export default function Screen({screenDisplay}) {
+export default function Screen({screenDisplay, acClicked}) {
     return (
         <section id="screenSection">
             <div id="screen"> {/* actual screen */}
                 <p>{screenDisplay}</p>
             </div>
             <div id="acBtnDiv">
-                <button>AC</button> {/* ALL CLEAR key */}
+                <button onClick={acClicked}>AC</button> {/* ALL CLEAR key */}
             </div>
         </section>
     )
